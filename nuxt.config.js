9f4669ddefffb7ddca16c55919f06c59d499e201
ghost-raven1 <sonofagronom@gmail.com>
2021-01-07 01:34:51 +0700

    0.1.1 Offline

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: '~/plugins/uikit.js', ssr: false }
+    { src: '~/plugins/uikit.js', ssr: false },
+    { src: '~/plugins/offline.js', ssr: false }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
