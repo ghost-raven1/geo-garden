48a7ee2479487390febbe139d8a8e9d93cdc178d
ghost-raven1 <sonofagronom@gmail.com>
2021-05-03 12:55:38 +0700

    Добавление контейнера для Tabel.vue

--- components/tabel/Tabel.vue
+++ components/tabel/Tabel.vue
@@ -1,10 +1,12 @@
 <template>
-  <iframe
-    src="https://calendar.google.com/calendar/embed?src=q9jlesm4vgdb0ihcndrh8jgl6s%40group.calendar.google.com&ctz=Asia%2FKrasnoyarsk"
-    style="border: 0"
-    width="800"
-    height="600"
-    frameborder="0"
-    scrolling="no"
-  />
+  <div class="adaptive_container">
+    <iframe
+      src="https://calendar.google.com/calendar/embed?src=q9jlesm4vgdb0ihcndrh8jgl6s%40group.calendar.google.com&ctz=Asia%2FKrasnoyarsk"
+      style="border: 0"
+      width="100%"
+      height="400"
+      frameborder="0"
+      scrolling="no"
+    />
+  </div>
 </template>
